583a57693bf45ff2fb0593fa663b2469bf253433
Hiikss <91778476+Hiikss@users.noreply.github.com>
2021-12-12 15:46:59 +0100

    log

--- src/log4j2/TestLog4J.java
+++ src/log4j2/TestLog4J.java
@@ -1,17 +1,22 @@
 package log4j2;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class TestLog4J {
 
     // Récupération de notre logger.
-    private static final Logger LOGGER =  LogManager.getLogger( "Compo1" );
+	private static final Logger LOGGER =  LogManager.getLogger(TestLog4J.class);
 
     // Le point d'entrée du programme.
-    public static void main( String [] args ) {
+    public static void main(String [] args) {
 
-        // TODO: utiliser votre logger
+    	// On produit un log de niveau informatif.
+        LOGGER.log( Level.INFO, "Hello World with Log4J 2" );
+
+        // On produit un log de niveau erreur.
+        LOGGER.log( Level.ERROR, "Houston, we have a problem" );
         
     }
 }
