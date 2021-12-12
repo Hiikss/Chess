c3e8ae05a12439712fd9176658b81c911642fe85
Hiikss <91778476+Hiikss@users.noreply.github.com>
2021-12-12 15:22:57 +0100

    ajout package + class pour log4j

---
+++ src/log4j2/TestLog4J.java
@@ -0,0 +1,17 @@
+package log4j2;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class TestLog4J {
+
+    // Récupération de notre logger.
+    private static final Logger LOGGER =  LogManager.getLogger( "Compo1" );
+
+    // Le point d'entrée du programme.
+    public static void main( String [] args ) {
+
+        // TODO: utiliser votre logger
+        
+    }
+}
