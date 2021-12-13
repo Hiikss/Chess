03b547ef4a57ba9311506c8f26fd94c7c5962940
VoiD7965 <guerineau.remi@gmail.com>
2021-12-13 23:01:48 +0100

    Create CheckedTest.java
    debut class test

---
+++ src/model/CheckedTest.java
@@ -0,0 +1,19 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CheckedTest {
+	
+	Checked c = new Checked();
+	Main d = new Main();
+	
+	/*
+	@Test
+	void testIsWhiteKingAttacked() {
+		Assertions.assertEquals(true, Checked.isWhiteKingAttacked(300, 100));
+	}
+	*/
+}
