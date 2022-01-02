771966270ce7512b971725893bfecda630128100
VoiD7965 <53878357+VoiD7965@users.noreply.github.com>
2022-01-02 17:20:16 +0100

    Update Checkmate.java

--- src/model/Checkmate.java
+++ src/model/Checkmate.java
@@ -6,7 +6,7 @@ import javax.swing.JButton;
 
 /** 
  * La classe <b>Checkmate</b> appartient au package <b>model</b>.
- * Elle comprend 2 méthodes vérifiant si un roi est en échec et mat
+ * Elle comprend 2 méthodes vérifiant si un roi est en échec et mat.
  * @see <b>isWhiteKingCheckmate<b>, <b>isBlackKingCheckmate<b>
  */
 
