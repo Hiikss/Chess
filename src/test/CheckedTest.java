36071f163c2e0bbc0a285e47fc0a98181e644285
VoiD7965 <53878357+VoiD7965@users.noreply.github.com>
2022-01-02 17:16:04 +0100

    Create CheckedTest.java

---
+++ src/test/CheckedTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import model.Checked;
+import model.Main;
+
+class CheckedTest {
+	
+	Checked c = new Checked();
+	Main d = new Main();
+	
+	/*
+	@Test
+	void testIsWhiteKingAttacked() {
+		Assertions.assertEquals(true, Checked.isWhiteKingAttacked(300, 100));
+	}
+	*/
+}
