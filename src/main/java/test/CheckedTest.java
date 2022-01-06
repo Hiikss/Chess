4e80a286041b2717be935d115adb20dfba0f534e
Hiikss <91778476+Hiikss@users.noreply.github.com>
2022-01-06 09:28:51 +0100

    mise en place de maven

--- src/main/java/test/CheckedTest.java
+++ src/main/java/test/CheckedTest.java
@@ -1,22 +1,21 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import model.Checked;
 import model.Main;
 
+@SuppressWarnings("unused")
 class CheckedTest {
 	
 	Checked c = new Checked();
 	Main d = new Main();
 	
-	/*
-	@Test
+	
+	/*@Test
 	void testIsWhiteKingAttacked() {
 		Assertions.assertEquals(true, Checked.isWhiteKingAttacked(300, 100));
-	}
-	*/
+	}*/
+	
 }
