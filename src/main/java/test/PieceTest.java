e487d38e5d09efa6047d41b20c5802b563c3b303
VoiD7965 <53878357+VoiD7965@users.noreply.github.com>
2022-01-23 16:44:37 +0100

    Create PieceTest.java

---
+++ src/main/java/test/PieceTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import model.Piece;
+
+class PieceTest {
+
+	Piece c = new Piece();
+	/*
+	@Test
+	void testGetImage() {	//Fonction de test vérifiant l'image attribuée à la valeur de la piece
+		Assertions.assertEquals(Piece.ipb, Piece.getImage(1));
+		Assertions.assertEquals(Piece.icb, Piece.getImage(2));
+		Assertions.assertEquals(Piece.ifb, Piece.getImage(3));
+		Assertions.assertEquals(Piece.itb, Piece.getImage(5));
+		Assertions.assertEquals(Piece.idb, Piece.getImage(9));
+		Assertions.assertEquals(Piece.irb, Piece.getImage(10));
+		Assertions.assertEquals(Piece.ipn, Piece.getImage(-1));
+		Assertions.assertEquals(Piece.icn, Piece.getImage(-2));
+		Assertions.assertEquals(Piece.ifn, Piece.getImage(-3));
+		Assertions.assertEquals(Piece.itn, Piece.getImage(-5));
+		Assertions.assertEquals(Piece.idn, Piece.getImage(-9));
+		Assertions.assertEquals(Piece.irn, Piece.getImage(-10));
+	}
+	*/
+}
