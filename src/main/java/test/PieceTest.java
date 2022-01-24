89ff8eceb21ab3bc4ea4a730f3560f0522793bb3
Hiikss <91778476+Hiikss@users.noreply.github.com>
2022-01-24 19:39:47 +0100

    commit

--- src/main/java/test/PieceTest.java
+++ src/main/java/test/PieceTest.java
@@ -1,6 +1,6 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.swing.ImageIcon;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,22 +9,36 @@ import model.Piece;
 
 class PieceTest {
 
-	Piece c = new Piece();
-	/*
+	Piece piece = new Piece();
+	
+	private ImageIcon ipb = new ImageIcon(Piece.class.getResource("/pb.png"));
+	private ImageIcon idb = new ImageIcon(Piece.class.getResource("/db.png"));
+	private ImageIcon irb = new ImageIcon(Piece.class.getResource("/rb.png"));
+	private ImageIcon ifb = new ImageIcon(Piece.class.getResource("/fb.png"));
+	private ImageIcon icb = new ImageIcon(Piece.class.getResource("/cb.png"));
+	private ImageIcon itb = new ImageIcon(Piece.class.getResource("/tb.png"));
+	
+	private ImageIcon ipn = new ImageIcon(Piece.class.getResource("/pn.png"));
+	private ImageIcon idn = new ImageIcon(Piece.class.getResource("/dn.png"));
+	private ImageIcon irn = new ImageIcon(Piece.class.getResource("/rn.png"));
+	private ImageIcon ifn = new ImageIcon(Piece.class.getResource("/fn.png"));
+	private ImageIcon icn = new ImageIcon(Piece.class.getResource("/cn.png"));
+	private ImageIcon itn = new ImageIcon(Piece.class.getResource("/tn.png"));
+	
 	@Test
-	void testGetImage() {	//Fonction de test vérifiant l'image attribuée à la valeur de la piece
-		Assertions.assertEquals(Piece.ipb, Piece.getImage(1));
-		Assertions.assertEquals(Piece.icb, Piece.getImage(2));
-		Assertions.assertEquals(Piece.ifb, Piece.getImage(3));
-		Assertions.assertEquals(Piece.itb, Piece.getImage(5));
-		Assertions.assertEquals(Piece.idb, Piece.getImage(9));
-		Assertions.assertEquals(Piece.irb, Piece.getImage(10));
-		Assertions.assertEquals(Piece.ipn, Piece.getImage(-1));
-		Assertions.assertEquals(Piece.icn, Piece.getImage(-2));
-		Assertions.assertEquals(Piece.ifn, Piece.getImage(-3));
-		Assertions.assertEquals(Piece.itn, Piece.getImage(-5));
-		Assertions.assertEquals(Piece.idn, Piece.getImage(-9));
-		Assertions.assertEquals(Piece.irn, Piece.getImage(-10));
+	void testGetImage() {	//Fonction de test vï¿½rifiant l'image attribuï¿½e ï¿½ la valeur de la piece
+		Assertions.assertEquals(ipb, piece.getImage(1));
+		Assertions.assertEquals(icb, piece.getImage(2));
+		Assertions.assertEquals(ifb, piece.getImage(3));
+		Assertions.assertEquals(itb, piece.getImage(5));
+		Assertions.assertEquals(idb, piece.getImage(9));
+		Assertions.assertEquals(irb, piece.getImage(10));
+		Assertions.assertEquals(ipn, piece.getImage(-1));
+		Assertions.assertEquals(icn, piece.getImage(-2));
+		Assertions.assertEquals(ifn, piece.getImage(-3));
+		Assertions.assertEquals(itn, piece.getImage(-5));
+		Assertions.assertEquals(idn, piece.getImage(-9));
+		Assertions.assertEquals(irn, piece.getImage(-10));
 	}
-	*/
+	
 }
